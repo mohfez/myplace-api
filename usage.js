cea09ac8366d31bfdd15527cdc662ad9514ff7dc
mohfez <150836596+mohfez@users.noreply.github.com>
2024-02-19 14:55:19 +0000

    small fix
    forgot await in the example

--- usage.js
+++ usage.js
@@ -13,7 +13,7 @@ async function Example()
 
     /*
     // example on creating a barcode:
-    barcode_gen.createStudentBarcode(libraryNumber, "./student_card/barcode.png");
+    await barcode_gen.createStudentBarcode(libraryNumber, "./student_card/barcode.png");
     await Sleep(3000);
     */
 
